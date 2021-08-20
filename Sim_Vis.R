4759c9f9c758b5d3d509051bafe142c843984b10
Travis Edward Rooney <ter56@cornell.edu>
2021-08-20 16:19:20 -0400

    more work and presenation things

--- Sim_Vis.R
+++ Sim_Vis.R
@@ -10,106 +10,204 @@ library(GAPIT3)
 library(tidyverse)
 library(readxl)
 library(purrrlyr)
+library(rrBLUP)
 
-# Let's start with the example we saw in the slides 5 loci, equal effect sizes.
-small_loci_num = 5
-P1_sl = rep(2,5)
-P2_sl = rep(0,5)
+# Let's start with the example we saw in the slides: 5 loci, equal effect sizes.
+P1_sl = rep(2,5) #P1 haplotype always carrying the 2
+P2_sl = rep(0,5) #P2 haplotype always carrying the 0
 
-numOffspring_sl = 200
-numLoci_sl = 5
-
-offspring_sl = data.frame()
-for (i in 1:numOffspring_sl){
-  for (j in 1:numLoci_sl){
-    temp = sum(temp,sample(c(P1_sl[j],P2_sl[j]),1))
-  }
-  offspring_sl = rbind(offspring_sl, data.frame (ProgNum = i, value = temp))
-  temp = 0
+numOffspring_sl = 200 #number of offspring to generate
+numLoci_sl = 5  # our 5 loci!
+Parents_sl = matrix(c(P1_sl,P2_sl), nrow = 5, ncol = 2)
+offspring_sl = matrix(NA, nrow = 5, ncol = 200)
+for (row in 1:nrow(Parents_sl)){
+  ithLocus = sample(c(Parents_sl[row,]),200, replace = T)
+  offspring_sl[row,] = ithLocus
 }
-offspring_sl %>% ggplot(aes(x =value)) +geom_histogram(bins = 6)+labs(title = '5 loci segregating, equal effect')
+
+markerEffects_SL = rep(1,5)
+offspring_sl_BV = data.frame(BV = t(t(markerEffects_SL)%*%offspring_sl ))
+Parents_sl_BV = data.frame(BV = t(t(markerEffects_SL)%*%Parents_sl ))
+# If you'd like to look at what this is doing as a for loop here it is essentially:
+# temp = 0
+# offspring_sl_BV = data.frame()
+# for (i in 1:numOffspring_sl){
+#   for (j in 1:numLoci_sl){
+#     temp = sum(temp,sample(c(P1_sl[j],P2_sl[j]),1))
+#   }
+#   offspring_sl = rbind(offspring_sl, data.frame (ProgNum = i, value = temp))
+#   temp = 0
+# }
+# Plot the results
+offspring_sl_BV %>% ggplot(aes(x =BV)) +geom_histogram(bins = 6)+labs(title = '5 loci segregating, equal effect')
+# This is the same as a binomial distribution with n = 5, k = number positive loci, p=q=0.5
 
 # Now lets see what happens with effect sizes that differ
 # lets create a vector of marker allele substitution effects
-MarkerEffects_sl = c(1,6,2,1,1)
+DifMarkerEffects_sl = c(1,6,2,1,1)
 # Same thing as above - play with changing the effect sizes and see what happens
-numOffspring_sl = 300
-offspring_sl_Ef = data.frame()
-numLoci_sl = 5
+offspring_SLD_BV = data.frame(BV = t(t(DifMarkerEffects_sl)%*%offspring_sl ))
+Parents_SLD_BV = data.frame(BV = t(t(DifMarkerEffects_sl)%*%Parents_sl ))
+# Plot the results
+offspring_SLD_BV %>% ggplot(aes(x =BV)) +geom_histogram(bins = 6)+labs(title = '5 loci segregating, differnt effect size')
 
-for (i in 1:numOffspring_sl){
-  for (j in 1:numLoci_sl){
-    temp = sum(temp,sample(c(P1_sl[j],P2_sl[j]),1)*MarkerEffects_sl[j])
-  }
-  offspring_sl_Ef = rbind(offspring_sl_Ef, data.frame (ProgNum = i, value = temp))
-  temp = 0
-}
-offspring_sl_Ef %>% ggplot(aes(x =value)) +geom_histogram(bins = 12)+labs(title = '5 loci segregating, different marker effects')
-
+###### MORE MARKERS! ######
 # So now lets extend things: 
-# A vector of 2's and 0's (fully inbred) is the same as a vector of 1's and 0's and is a little easier to think about
-# Lets say now we have 200 loci, with equal effect sizes, but now there are some sites the parents have in common 
+# A vector of 2's and 0's (fully inbred) is the same as a vector of 1's and 0's (just divide by 2) and is a little easier to think about.
+# Lets say now we have 200 loci, but now there are some sites the parents have in common 
 # (ie the sites are not segregating), and P2 no loger has only the negative allele. 
 numLoci = 200
-# 
-# P1 = rep(1,numLoci)
-# P2 = rep(0,numLoci)
+# Lets create parents with more loci
+P1 = c(sample(c(0,1), numLoci-1, replace = T,prob = c(0.5,0.5)),1)
+P2 = c(sample(c(0,1), numLoci-1, replace = T,prob = c(0.5,0.5)),0)
 
-P1 = sample(c(0,1), numLoci, replace = T,prob = c(0.5,0.5))
-P2 = sample(c(0,1), numLoci, replace = T,prob = c(0.5,0.5))
-
-numOffspring = 500
-offspring = data.frame() 
-temp=0
-
-# mutate(x1 = sample(P1,P2))
-
-for (i in 1:numOffspring){
-  for (j in 1:numLoci){
-    temp = sum(temp,sample(c(P1[j],P2[j]),1))
-  }
-  offspring = rbind(offspring, data.frame (ProgNum = i, value = temp))
-  temp = 0
+#put those parents into a matrix
+ParentMatrix = matrix(c(P1,P2), nrow = numLoci, ncol = 2)
+numOffspring = 500 #decide on the number of offspinrg to simulate
+offspring = matrix(NA, nrow = numLoci, ncol = numOffspring) #and make a matrix with that size.
+# We will populate the offspirng matrix with the offspring each locus at a time (as things are unlinked) using this 'for' loop:
+for (row in 1:nrow(ParentMatrix)){
+  ithLocus = sample(c(ParentMatrix[row,]),numOffspring, replace = T)
+  offspring[row,] = ithLocus
 }
-offspring %>% ggplot(aes(x = value))+geom_density()+geom_vline(xintercept = c(sum(P1),sum(P2))) +
-  geom_label(data = data.frame(y = c(0.05,0.05), x = c(sum(P1),sum(P2)), label = c('P1','P2')),
+# which makes a matrix with the offsping in columns, and the loci in rows!
+# now let's use this matrix to examine offsping values: 
+# Let's assume equal effect sizes, 1 being positive and 0 being the negative. 
+# That is for each offsping we are going to find the sum of the marker effects, assign that 
+# as the individuals breeding value and then plot them!
+# All loci have equal effects, so lets assing a vector of '1's as the effect size (this is that vector of a's that we saw on the slide)
+Equaleffect_markers = rep(1,numLoci)
+Parent_BV = data.frame(BV = t(Equaleffect_markers %*% ParentMatrix)) #
+Offspring_BV =  data.frame(BV = t(Equaleffect_markers %*% offspring))
+Offspring_BV%>% ggplot(aes(x = BV))+geom_density()+geom_vline(xintercept = Parent_BV$BV) +
+  geom_label(data = data.frame(y = c(0.05,0.05), x = Parent_BV$BV, label = c('P1','P2')),
              aes(x =x, y = y, label = label))
 
-P1s = c(sample(c(0,1), numLoci, replace = T), 20)
-P2s = c(sample(c(0,1), numLoci, replace = T), 0)
+# Now lets do the same thing, but include a Single Large Effect (SLE) locus at the end of the parnetal haplotypes that 
+# the parents are segregating for - see above where P1 and P2 are inititalized
+SLE_markerEffect = c(rep(1,numLoci-1),20)
+Parent_SLE_BV = data.frame(BV = t(t(SLE_markerEffect) %*% ParentMatrix))
+Offspring_SLE_BV = data.frame(BV = t(t(SLE_markerEffect) %*% offspring))
 
-offsprings = data.frame() 
-temp=0
-for (i in 1:numOffspring){
-  for (j in 1:numLoci+1){
-    temp = sum(temp,sample(c(P1s[j],P2s[j]),1))
-  }
-  offsprings = rbind(offsprings, data.frame (ProgNum = i, value = temp))
-  temp = 0
-}
-sinlgeLargeEffect = offsprings %>% ggplot(aes(x = value))+geom_density()+geom_vline(xintercept = c(sum(P1s),sum(P2s))) +
-  geom_label(data = data.frame(y = c(0.05,0.05), x = c(sum(P1s),sum(P2s)), label = c('P1','P2')),
+Offspring_SLE_BV %>% ggplot(aes(x = BV))+geom_density()+geom_vline(xintercept = Parent_SLE_BV$BV) +
+  geom_label(data = data.frame(y = c(0.05,0.05), x = Parent_SLE_BV$BV, label = c('P1','P2')),
              aes(x =x, y = y, label = label))
 
 
-P1ranef = sample(c(0,1), numLoci, replace = T)
-P2ranef = sample(c(0,1), numLoci, replace = T)
-MarkerEf = rnorm(numLoci, mean = 1)
 
-offspringranef = data.frame() 
-temp=0
-for (i in 1:numOffspring){
-  for (j in 1:numLoci){
-    temp = sum(temp,sum((sample(c(P1s[j],P2s[j]),1)))*MarkerEf[j])
-  }
-  offspringranef = rbind(offspringranef, data.frame (ProgNum = i, value = temp))
-  temp = 0
-}
-ranefPop = offspringranef %>% ggplot(aes(x = value))+geom_density()+geom_vline(xintercept = c(sum(P1ranef*MarkerEf),sum(P2ranef*MarkerEf))) +
-  geom_label(data = data.frame(y = c(0.05,0.05), x = c(sum(P1ranef*MarkerEf),sum(P2ranef*MarkerEf)), label = c('P1','P2')),
+
+# Now what happens when we assign a random effect size to all the markers (ie marker effects are not constant) 
+# in this the effects are drawn from a normal distribution with mean and sd of 1. 
+# The parental haplotype now inditcates presence/absence!
+Random_MarkerEffect = rnorm(numLoci, mean = 1, sd= 1)
+Parent_Randef_BV = data.frame(BV = t(t(Random_MarkerEffect) %*% ParentMatrix))
+Offspring_Randef_BV = data.frame(BV = t(t(Random_MarkerEffect) %*% offspring))
+
+Offspring_Randef_BV %>% ggplot(aes(x = BV))+geom_density()+geom_vline(xintercept = Parent_Randef_BV$BV) +
+  geom_label(data = data.frame(y = c(0.05,0.05), x = Parent_Randef_BV$BV, label = c('P1','P2')),
              aes(x =x, y = y, label = label))
 
-Norm+sinlgeLargeEffect+ranefPop +plot_layout(ncol = 1)
+
+x = sample(c(1,0),100,replace = T)
+y = rnorm(mean = x*6, sd=1, n = 100)
+data.frame(x = x,y=y) %>% ggplot(aes(x =x, y =y))+geom_point() +xlab('Marker Score')+
+  ylab('Breeding Value') +geom_smooth(formula = y~x, se = F, method = lm)
+
+
+
+############## Let use the random vs fixed effects models and see how they respond
+# Lets also write a function to give us the BV, marker matrix, and etc:
+GenerateBiparental = function(NumLoci, PopulationSize, MarkerEffects){
+  ParentMatrix = matrix(sample(c(0,1),NumLoci*2,replace = T), nrow = NumLoci, ncol = 2)
+  Offspring = matrix(NA, nrow = NumLoci, ncol = PopulationSize) 
+  ExtraOffspring = matrix(NA, nrow = NumLoci, ncol = PopulationSize)#and make a matrix with that size.
+  # We will populate the offspirng matrix with the offspring each locus at a time (as things are unlinked) using this 'for' loop:
+  for (row in 1:nrow(ParentMatrix)){
+    ithLocus = sample(c(ParentMatrix[row,]),PopulationSize, replace = T)
+    Offspring[row,] = ithLocus
+    ithLocus2 = sample(c(ParentMatrix[row,]),PopulationSize, replace = T)
+    ExtraOffspring[row,] = ithLocus2
+  }
+  Parent_BV = data.frame(BV = t(MarkerEffects %*% ParentMatrix)) #
+  Offspring_BV =  data.frame(BV = t(MarkerEffects %*% Offspring))
+  ExtraOffspring_BV =  data.frame(BV = t(MarkerEffects %*% ExtraOffspring))
+  
+  Plot = Offspring_BV %>% ggplot(aes(x = BV))+geom_density()+geom_vline(xintercept = Parent_BV$BV) +
+    geom_label(data = data.frame(y = c(0.05,0.05), x = Parent_BV$BV, label = c('P1','P2')),
+               aes(x =x, y = y, label = label))
+  
+  return(list(ParentMatrix =ParentMatrix,
+              Parent_BV = Parent_BV,
+              Offspring_BV = Offspring_BV,
+              MarkerEffects = MarkerEffects,
+              Offspring = Offspring,
+              Plot = Plot,
+              ExtraOffspring = ExtraOffspring,
+              ExtraOffspring_BV = ExtraOffspring_BV))
+  
+}
+
+
+library(rrBLUP)
+# 5 loci with numerous individuals works well
+t(offspring_sl) %>% data.frame() %>% cbind(., offspring_sl_BV$BV) %>%
+  rename(BV = 'offspring_sl_BV$BV') %>%
+  lm(BV ~ X1+X2+X3+X4+X5, data = .)
+
+# now 200 markers with 500 individuals - equal effect markers remember
+t(offspring) %>% data.frame() %>% cbind(., Offspring_BV$BV) %>%
+  rename(BV = 'Offspring_BV$BV') %>%
+  lm(BV ~ . , data = .)
+# works well everything was one remember
+
+# How about about normally distributed markers?
+randeffect.lm = t(offspring) %>% data.frame() %>% cbind(., Offspring_Randef_BV$BV) %>%
+  rename(BV = 'Offspring_Randef_BV$BV') %>%
+  lm(BV ~ . , data = .)
+# lets check correltion of marker effects.
+Random_MarkerEffect %>% cbind(data.frame(randeffect.lm$coefficients)[-1,]) %>% cor(., use = 'complete.obs')
+# That works well to - mono morphic markers are dropped from the equation, so really we only have about 100 markers 
+# that we are estimating. ie N > P by 5x
+
+#now lets get more realistic
+P4000_N500 = GenerateBiparental(NumLoci = 4000,PopulationSize = 500, MarkerEffects = rnorm(4000,mean = 0, sd = 1))
+P4000_N500$Plot
+P4k_N500.lm = t(P4000_N500$Offspring) %>% data.frame() %>%cbind(., P4000_N500$Offspring_BV) %>%
+  lm(BV~.,data = .)
+P4000_N500$MarkerEffects %>% cbind(data.frame(P4k_N500.lm$coefficients)[-1,]) %>% cor(., use = 'complete.obs')
+# No correlation between estimated effects and actual known effect!
+MarkerCoef4k_n500= as.data.frame(P4k_N500.lm$coefficients) #let get out the coef
+MarkerCoef4k_n500[is.na(MarkerCoef4k_n500)] <- 0 #replace nas with 0s so that things can be calcuated  - notice there are 499 of them 
+cor(P4000_N500$ExtraOffspring_BV$BV,#TrueValues
+    t(matrix(MarkerCoef4k_n500$`P4k_N500.lm$coefficients`)[-1,]  %*% P4000_N500$ExtraOffspring))#What we calcuate
+#NO Correlation! everything is over fit and underwhelming!
+
+# Now what if we used a ridge regression process to regularize and make the predictors less sensitive?
+# rrblup is a good framework to work through 
+Ueffects = mixed.solve(y = P4000_N500$Offspring_BV$BV, Z = t(P4000_N500$Offspring))
+#Now lets check to see if these marker effects along with the extra offspring simulated and there true breeding values are correlated
+cor(t(as.matrix(P4000_N500$ExtraOffspring)) %*% as.matrix(Ueffects$u),P4000_N500$ExtraOffspring_BV$BV)
+#~0.5 which is pretty dang good. 
+# Lets look at the Markers now and the estimated vs actual effects:
+data.frame(Estimated = as.matrix(Ueffects$u), Known =P4000_N500$MarkerEffects) %>%
+  ggplot(aes(x = Known, y = Estimated))+geom_point() #Markers that are monomorphis get estimate effect sizes of zero hence those at zero
+
+
+
+numP = 15
+x = rnorm(numP, sd = 4)
+y = rnorm(numP,mean = x)
+lm(y~x)
+data.frame(resid = y,my = mean(y),x=x) %>% mutate(point = 1:numP) %>% pivot_longer(cols =c(resid,my)) %>%
+  ggplot()+geom_line(aes(x = x, y = value, group =point))+geom_point(data = data.frame(x = x,y=y), aes(x=x,y=y))+
+  geom_hline(yintercept = mean(y))
+  
+data.frame(resid = y-(.5*x+mean(y)),my = .5*x+mean(y),x=x) %>% mutate(point = 1:numP) %>% pivot_longer(cols =c(resid,my)) %>%
+  ggplot()+geom_line(aes(x = x, y = value, group =point))+geom_point(data = data.frame(x = x,y=y), aes(x=x,y=y))+
+  geom_abline(slope = .5 )
+
+
+
 
 
 
